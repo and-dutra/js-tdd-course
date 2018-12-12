639e16d1f161f6e5ee8220fcd60ed7cbf899ca81
Andre <aoliveira@tray.net.br>
2018-12-12 16:53:53 -0200

    Testes de modelo

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,20 @@
-import { expect } from 'chai';
+import { expect } from "chai";
 
-describe('Main', () => {
+describe("Main", () => {
+  describe("Method A", function() {
+    context("Case 1", function() {
+      it("Should happen bla", function() {
+        //roda o teste aqui
+      });
+    });
+    context("Case 2", function() {
+      it("Should happen bla bla", function() {
+        //roda o teste aqui
+      });
+    });
+  });
 
+  describe("Method B", function() {
+    //bla bla bal
+  });
 });
